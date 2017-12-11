983db5e790f9e8693f9fc04e584f2209ef2706fa
EL TAHHAN Sana <sana.eltahhan@hotmail.fr>
2017-12-11 12:52:45 +0100

    Basic login html
    Login + csrf

---
+++ src/main/java/com/miage/m2/webappforum/Control/ControllerLogin.java
@@ -0,0 +1,13 @@
+package com.miage.m2.webappforum.Control;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@org.springframework.stereotype.Controller
+public class ControllerLogin {
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String login() {
+        return "login/login";
+    }
+}
