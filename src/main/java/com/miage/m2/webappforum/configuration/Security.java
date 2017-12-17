cac8473ddc25118a7ea7a4fab7a640bb98c638a1
arnaudfreismuth <arnaudfreismuth@gmail.com>
2017-12-17 20:29:06 +0100

    change encryption with a bean in configuration.security

--- src/main/java/com/miage/m2/webappforum/configuration/Security.java
+++ src/main/java/com/miage/m2/webappforum/configuration/Security.java
@@ -1,13 +1,17 @@
 package com.miage.m2.webappforum.configuration;
 
 import javax.sql.DataSource;
+import org.apache.commons.codec.digest.DigestUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -17,12 +21,16 @@ public class Security extends WebSecurityConfigurerAdapter {
   DataSource dataSource;
 
   @Autowired
+  PasswordEncoder passwordEncoder;
+
+  @Autowired
   protected void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
     auth.inMemoryAuthentication()
         .withUser("user").password("password")
         .roles("USER").and().withUser("admin").password("admin").roles("ADMIN");
     auth.jdbcAuthentication()
         .dataSource(dataSource)
+        .passwordEncoder(passwordEncoder)
         .usersByUsernameQuery("SELECT pseudo, password, 1 FROM utilisateur WHERE pseudo=?")
         .authoritiesByUsernameQuery("select pseudo, 'USER' from utilisateur where pseudo=?");
 
@@ -41,5 +49,10 @@ public class Security extends WebSecurityConfigurerAdapter {
         .and()
         .csrf();
 
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
   }
 }
--- src/main/java/com/miage/m2/webappforum/control/RegisterController.java
+++ src/main/java/com/miage/m2/webappforum/control/RegisterController.java
@@ -2,8 +2,11 @@ package com.miage.m2.webappforum.control;
 
 import com.miage.m2.webappforum.entity.Utilisateur;
 import com.miage.m2.webappforum.repository.UtilisateurRepository;
+import java.util.Date;
+import javax.validation.Valid;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -12,44 +15,46 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.validation.Valid;
-import java.util.Date;
-
 @Controller
 public class RegisterController {
 
-    @Autowired
-    UtilisateurRepository ur;
+  @Autowired
+  UtilisateurRepository ur;
 
-    @GetMapping(value = "/register")
-    public String askAddUser(Model model) {
-        model.addAttribute("user", new Utilisateur());
-        return "login/register";
+  @Autowired
+  private PasswordEncoder passwordEncoder;
+
+  @GetMapping(value = "/register")
+  public String askAddUser(Model model) {
+    model.addAttribute("user", new Utilisateur());
+    return "login/register";
+  }
+
+
+  @PostMapping(value = "/register")
+  public String addUser(Model model, @ModelAttribute("user") @Valid Utilisateur user,
+      BindingResult result,
+      @RequestParam(value = "password_confirm") String confirm) {
+    if (!confirm.equals(user.getPassword())) {
+      result.rejectValue("password", "notSamePassword");
     }
-
-
-    @PostMapping(value = "/register")
-    public String addUser(Model model, @ModelAttribute("user") @Valid Utilisateur user, BindingResult result,
-                          @RequestParam(value = "password_confirm") String confirm) {
-        if (!confirm.equals(user.getPassword())) {
-            result.rejectValue("password", "notSamePassword");
-        }
-        if (ur.existsByPseudo(user.getPseudo())) {
-            result.rejectValue("pseudo", "register.userNameAlreadyExist");
-        }
-        if (ur.existsByEmail(user.getEmail())) {
-            result.rejectValue("email", "register.mailAlreadyExist");
-        }
-        if (result.hasErrors()) {
-            model.addAttribute("user", user);
-            return "login/register";
-        } else {
-            user.setInscription(new Date());
-            String md5Hex = DigestUtils.md5Hex(user.getPassword()).toUpperCase();
-            user.setPassword(md5Hex);
-            ur.save(user);
-            return "redirect:/login";
-        }
+    if (ur.existsByPseudo(user.getPseudo())) {
+      result.rejectValue("pseudo", "register.userNameAlreadyExist");
     }
+    if (ur.existsByEmail(user.getEmail())) {
+      result.rejectValue("email", "register.mailAlreadyExist");
+    }
+    if (result.hasErrors()) {
+      model.addAttribute("user", user);
+      return "login/register";
+    } else {
+      user.setInscription(new Date());
+//      String md5Hex = DigestUtils.md5Hex(user.getPassword()).toUpperCase();
+//      user.setPassword(md5Hex);
+      user.setPassword(passwordEncoder.encode(user.getPassword()));
+      ur.save(user);
+      return "redirect:/login";
+    }
+  }
 
 }
