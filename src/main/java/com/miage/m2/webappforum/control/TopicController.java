837401f24b1dc5f7dfb0c5c5c1e5a3059b892009
Anne Jacquet <anne-jacquet@sfr.fr>
2017-12-19 23:56:40 +0100

    fix following topic - onclick replaced by form

--- src/main/java/com/miage/m2/webappforum/control/TopicController.java
+++ src/main/java/com/miage/m2/webappforum/control/TopicController.java
@@ -5,6 +5,7 @@ import com.miage.m2.webappforum.entity.Topic;
 import com.miage.m2.webappforum.entity.Utilisateur;
 import com.miage.m2.webappforum.repository.ProjetRepository;
 import com.miage.m2.webappforum.repository.TopicRepository;
+import com.miage.m2.webappforum.repository.UtilisateurRepository;
 import com.miage.m2.webappforum.service.UserService;
 import java.util.List;
 import java.util.Set;
@@ -13,6 +14,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class TopicController {
@@ -21,14 +23,16 @@ public class TopicController {
   TopicRepository tr;
   @Autowired
   ProjetRepository pr;
+  @Autowired
+  UtilisateurRepository ur;
 
   @Autowired
-  UserService ur;
+  UserService us;
 
 
   @GetMapping(value = "/followedTopics")
   public String getFollowedTopics(Model model) {
-    Utilisateur utilisateur = ur.getLoggedUser();
+    Utilisateur utilisateur = us.getLoggedUser();
     Set<Topic> topicList = utilisateur.getFollowTopicList();
     topicList.forEach(t -> t.setFollowedByUser(true));
     model.addAttribute("topicList", topicList);
@@ -37,12 +41,25 @@ public class TopicController {
 
   @GetMapping(value = "/projets/{id}/topics")
   public String getAllTopicsOfProject(@PathVariable("id") String id, Model model) {
-    Utilisateur utilisateur = ur.getLoggedUser();
+    Utilisateur utilisateur = us.getLoggedUser();
     Projet projet = pr.findOne(id);
     List<Topic> topicList = projet.getTopicList();
     topicList.forEach(t -> t.setFollowedByUser(t.getFollowerList().contains(utilisateur)));
     model.addAttribute("topicList", topicList);
     return "topic/topic";
+  }
+
+  @PostMapping(value = "/topics/{idTopic}/follow")
+  public String addTopicFollowing(@PathVariable("idTopic") String topicId) {
+    Topic topic = tr.findOne(topicId);
+    Utilisateur utilisateur = us.getLoggedUser();
+    if (utilisateur.getFollowTopicList().contains(topic)) {
+      utilisateur.getFollowTopicList().remove(topic);
+    } else {
+      utilisateur.getFollowTopicList().add(topic);
+    }
+    ur.save(utilisateur);
+    return "redirect:/followedTopics";
   }
 
 
