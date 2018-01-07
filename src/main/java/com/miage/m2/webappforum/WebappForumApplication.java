f4b465560c94b392ed80c35e508ba04fb9fcbcc0
arnaudfreismuth <arnaudfreismuth@gmail.com>
2018-01-07 16:25:34 +0100

    add datainitilize

--- src/main/java/com/miage/m2/webappforum/WebappForumApplication.java
+++ src/main/java/com/miage/m2/webappforum/WebappForumApplication.java
@@ -1,13 +1,33 @@
 package com.miage.m2.webappforum;
 
+import com.miage.m2.webappforum.entity.Role;
+import com.miage.m2.webappforum.repository.RoleRepository;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class WebappForumApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WebappForumApplication.class, args);
-	}
+  @Autowired
+  RoleRepository rr;
+
+  public static void main(String[] args) {
+    SpringApplication.run(WebappForumApplication.class, args);
+  }
+
+  @Bean
+  InitializingBean sendDatabase() {
+    return () -> {
+      Role r = rr.getRoleByName("USER");
+      if (r == null) {
+        Role role = new Role();
+        role.setName("USER");
+        rr.save(role);
+      }
+    };
+  }
+
 }
