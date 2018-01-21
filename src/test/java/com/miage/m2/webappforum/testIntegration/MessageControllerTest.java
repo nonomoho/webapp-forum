badc9110b58cef09453b09a8770db0c7884ec5bf
Alexandre Milev <alex92250@gmail.com>
2018-01-21 21:36:04 +0100

    test message controller hotfix

--- src/test/java/com/miage/m2/webappforum/testIntegration/MessageControllerTest.java
+++ src/test/java/com/miage/m2/webappforum/testIntegration/MessageControllerTest.java
@@ -17,8 +17,10 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.json.JacksonTester;
@@ -33,23 +35,24 @@ import javax.validation.constraints.AssertTrue;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashSet;
 
 import static org.codehaus.groovy.runtime.DefaultGroovyMethods.any;
+import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes = {WebappForumApplication.class})
+@RunWith(MockitoJUnitRunner.class)
 public class MessageControllerTest {
 
 
     private MockMvc mvc;
 
-    @Autowired
-    private ObjectMapper objectMapper;
+
+     ObjectMapper objectMapper = new ObjectMapper();
 
     MessageRepository mr = mock(MessageRepository.class);
 
@@ -66,23 +69,33 @@ public class MessageControllerTest {
 
     private Message messagePost;
 
-    @Autowired
-    UserService us;
+    private Topic topic;
+
+    private Projet projet;
+
+    @InjectMocks
+    MessageController msg;
+
+
+    UserService us = mock(UserService.class);
 
     @Before
     public void setUp() {
         JacksonTester.initFields(this, objectMapper);
-        MessageController msg = mock(MessageController.class);
-        this.mvc = standaloneSetup(msg).alwaysExpect(status().isOk()).build();
+
+        this.mvc = standaloneSetup(msg).build();
         Projet projet = new Projet();
         projet.setId("2");
         projet.setNom("COUCOU");
         projet.setDescription("coucou");
         pr.save(projet);
+        this.projet = projet;
         Topic topic = new Topic();
         topic.setProjet(projet);
-        topic.setId("2");
+        topic.setMessageList(new ArrayList<>());
+        topic.setFollowerList(new HashSet<>());
         tr.save(topic);
+        this.topic = topic;
       //  when(tr.findOne("2")).thenReturn(topic);
         Utilisateur user = new Utilisateur();
         user.setId("5");
@@ -90,6 +103,7 @@ public class MessageControllerTest {
         user.setPassword("coucou");
         user.setEmail("coucou@mail.com");
         ur.save(user);
+        when(us.getLoggedUser()).thenReturn(user);
         Message message = new Message();
         message.setContenu("post message");
         message.setDate(new Date());
@@ -99,25 +113,18 @@ public class MessageControllerTest {
     }
     @Test
     public void testGetMessage() throws Exception {
-
-        this.mvc.perform(get("/projets/2/topics/2/messages")).andExpect(view().name("projets/2/topics/2/messages"));
+        when(tr.findOne(anyString())).thenReturn(topic);
+        this.mvc.perform(get("/projets/" + projet.getId()+"/topics/" + topic.getId()  +"/messages")).andExpect(view().name("message/message"));
 
     }
     @Test
     public void testSaveMessage() throws  Exception{
-
+        when(tr.findOne(anyString())).thenReturn(topic);
         final String msgJson = jsonTester.write(messagePost).getJson();
-        this.mvc.perform(post("/projets/2/topics/2/messages/save")
+        this.mvc.perform(post("/projets/" + projet.getId()+"/topics/" + topic.getId()  +"/messages/save")
                 .contentType(MediaType.APPLICATION_JSON).content(msgJson))
-                .andExpect(view().name("projets/2/topics/2/messages/save")
-                ).andExpect(status().isOk());
-
-    }
-
-    @Test
-    public void testAddMessage() throws Exception {
-
-        this.mvc.perform(get("/projets/2/topics/2/messages/add")).andExpect(view().name("projets/2/topics/2/messages/add"));
+                .andExpect(view().name("redirect:/projets/{id}/topics/{idTopic}/messages")
+                ).andExpect(status().is3xxRedirection());
 
     }
 
