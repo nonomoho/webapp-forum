fd158e4ece239c670110183e48878f2709bdf834
Alexandre Milev <alex92250@gmail.com>
2018-01-21 17:08:52 +0100

    test service topicFollowing

--- src/test/java/com/miage/m2/webappforum/testServices/FollowServiceTest.java
+++ src/test/java/com/miage/m2/webappforum/testServices/FollowServiceTest.java
@@ -1,11 +1,13 @@
-package com.miage.m2.webappforum.testIntegration;
-
+package com.miage.m2.webappforum.testServices;
 
 import com.miage.m2.webappforum.WebappForumApplication;
-import com.miage.m2.webappforum.control.UtilisateurController;
+import com.miage.m2.webappforum.entity.Topic;
 import com.miage.m2.webappforum.entity.Utilisateur;
+import com.miage.m2.webappforum.repository.TopicRepository;
 import com.miage.m2.webappforum.repository.UtilisateurRepository;
+import com.miage.m2.webappforum.service.FollowService;
 import com.miage.m2.webappforum.service.UserService;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -18,32 +20,40 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.Date;
-
+import java.util.HashSet;
 
 import static junit.framework.TestCase.assertEquals;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(classes = {WebappForumApplication.class,UserService.class})
+@SpringBootTest(classes = {WebappForumApplication.class,FollowService.class,UserService.class})
 @DataJpaTest
-public class UtilisateurControllerTest {
+public class FollowServiceTest {
 
     @Autowired
-    private UserService userService;
+    FollowService followService;
 
     @Autowired
-    private UtilisateurRepository ur;
+    TopicRepository tr;
 
+    @Autowired
+    UtilisateurRepository ur;
 
-    private Utilisateur user;
+    @Autowired
+    UserService us;
+
+    Utilisateur user;
+
+    Topic topic;
 
     @Before
     public void onSetUp() throws Exception {
-        // add principal object to SecurityContextHolder
+
         Utilisateur user = new Utilisateur();
         user.setPseudo("test");
         user.setEmail("test@mail.com");
         user.setPassword("test");
         user.setId("123456");
+        user.setFollowTopicList(new HashSet<>());
         user.setInscription(new Date());
         this.user = user;
         ur.save(user);
@@ -53,11 +63,16 @@ public class UtilisateurControllerTest {
 
     }
 
-
     @Test
-    public void testLoggedUser() throws Exception {
-        Utilisateur loggedUser = userService.getLoggedUser();
-        assertEquals(loggedUser,user);
+    public void testTopicFollowing() throws Exception {
+        Topic topic = new Topic();
+        topic.setId("2");
+        tr.save(topic);
+        followService.addTopicFollowing("2");
+
+        Utilisateur utilisateur = us.getLoggedUser();
+        Assert.assertEquals(1,utilisateur.getFollowTopicList().size());
+
 
     }
 
